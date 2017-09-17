be9e6a3ac30adb4d7796add89b4497172521bf1a
yueyingqingshang <381265457@qq.com>
2017-09-17 12:12:18 +0800

    fix

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './progress.less';
 //	{ this.props.progress }s 
 let Progress = React.createClass({
-
+	changeProgress(e) {
+		let progressBar = this.refs.progressBar;
+		let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+		console.log(progress);
+	},
 	render() {
 		return (
-			<div className="components-progress">
-				<div className="progress" style ={{width: `${this.props.progress}%`}}></div>
+			<div className="components-progress" 
+				ref = "progressBar"
+				onClick = {this.changeProgress}>
+				<div className="progress" 
+				style ={{width: `${this.props.progress}%`}}
+				
+				></div>
 			</div>			
 		);
 	}
